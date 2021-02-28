2081610e7e7b7753c869e0b37629c9d1bbcb5294
Utsav <utsavp0213@gmail.com>
2021-02-28 17:49:18 +0530

    Added: Option for custom delimiter

--- src/main/java/com/calc/StringCalculator.java
+++ src/main/java/com/calc/StringCalculator.java
@@ -1,11 +1,24 @@
 package com.calc;
 
+import java.nio.file.LinkPermission;
+
 public class StringCalculator {
     public int add(String numbers) {
+        String nums = numbers;
+        String delimiter = ",|[\n|(\r\n)]"; //Delimiters - Either ',' or '\n' or '\r\n'
+        if (numbers.startsWith("//")) {
+            String[] input = numbers.split("\n", 2);
+            delimiter = input[0];
+            nums = input[1];
+        }
+        return add(nums, delimiter);
+    }
+
+    public int add(String numbers, String delimiter) {
         int sum = 0;
         if (numbers.isBlank())
             return sum;
-        String[] numbersSeparated = numbers.split(",|[\n|(\r\n)]");
+        String[] numbersSeparated = numbers.split(delimiter);
         for (String num: numbersSeparated)
             sum += Integer.parseInt(num);
         return  sum;
